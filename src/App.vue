9507b67922425c5b8614673e82fc7991435a831d
Chris <chrispackett1998@gmail.com>
2018-09-05 01:28:21 -0400

    finished explorer mode

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,7 @@
   <div id="app">
     <Logo />
     <HelloWorld message="Welcome to the Vue Scoreboard!"/>
-    <div class="teams">
-      <div class="team">
-        <Scoreboard v-bind:teamName="teamOneName" v-bind:teamScore="teamOneScore" />
-        <Update />
-      </div>
-      <div class="team">
-        <Scoreboard v-bind:teamName="teamTwoName" v-bind:teamScore="teamTwoScore" />
-        <Update />
-      </div>
-    </div>
+    <Scoreboard />
   </div>
 </template>
 
@@ -19,23 +10,13 @@
 import HelloWorld from './components/HelloWorld.vue'
 import Logo from './components/Logo.vue'
 import Scoreboard from './components/Scoreboard.vue'
-import Update from './components/Update.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
     Logo,
-    Scoreboard,
-    Update
-  },
-  data: function () {
-    return {
-      teamOneName: 'Team 1 Name',
-      teamOneScore: 0,
-      teamTwoName: 'Team 2 Name',
-      teamTwoScore: 0
-    }
+    Scoreboard
   }
 }
 </script>
@@ -48,14 +29,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 2em;
-}
-
-.teams {
-  display: flex;
-  justify-content: space-evenly;
-}
-
-.team-score {
-  font-size: 48px;
 }
 </style>
--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,16 +1,106 @@
 <template>
-  <div class="team-scoreboard-container">
-    <h4 class="team-name">{{ teamName }}</h4>
-    <h1 class="team-score">{{ teamScore }}</h1>
+  <div class="teams">
+    <div class="team">
+      <div class="team-scoreboard-container">
+        <h4 class="team-name">{{ teamOneName }}</h4>
+        <h1 class="team-score">{{ teamOneScore }}</h1>
+      </div>
+      <div class="team-update-container">
+        <div class="team-update-name">
+          <h4>Update Team Name:</h4>
+          <div class="team-update-name-input-and-button">
+            <input type="text" placeholder="Update team name..." v-model='teamOneNameInput'>
+            <button v-on:click='updateTeamOneName'>Update</button>
+          </div>
+        </div>
+        <div class="team-update-score">
+          <h4>Update Team Score:</h4>
+          <div class="team-update-score-buttons">
+            <button v-on:click='addOneToTeamOneScore'>Add 1</button>
+            <button v-on:click='subOneFromTeamOneScore'>Subtract 1</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="team">
+      <div class="team-scoreboard-container">
+        <h4 class="team-name">{{ teamTwoName }}</h4>
+        <h1 class="team-score">{{ teamTwoScore }}</h1>
+      </div>
+      <div class="team-update-container">
+        <div class="team-update-name">
+          <h4>Update Team Name:</h4>
+          <div class="team-update-name-input-and-button">
+            <input type="text" placeholder="Update team name..." v-model='teamTwoNameInput'>
+            <button v-on:click='updateTeamTwoName'>Update</button>
+          </div>
+        </div>
+        <div class="team-update-score">
+          <h4>Update Team Score:</h4>
+          <div class="team-update-score-buttons">
+            <button v-on:click='addOneToTeamTwoScore'>Add 1</button>
+            <button v-on:click='subOneFromTeamTwoScore'>Subtract 1</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Scoreboard",
-  props: {
-    teamName: String,
-    teamScore: Number
+  data: function () {
+    return {
+      teamOneName: 'Team 1 Name',
+      teamOneNameInput: '',
+      teamOneScore: 0,
+      teamTwoName: 'Team 2 Name',
+      teamTwoNameInput: '',
+      teamTwoScore: 0
+    }
+  },
+  methods: {
+    updateTeamOneName:function () {
+      this.teamOneName = this.teamOneNameInput
+      this.teamOneNameInput = ''
+    },
+    updateTeamTwoName:function () {
+      this.teamTwoName = this.teamTwoNameInput
+      this.teamTwoNameInput = ''
+    },
+    addOneToTeamOneScore:function () {
+      this.teamOneScore++
+    },
+    addOneToTeamTwoScore:function () {
+      this.teamTwoScore++
+    },
+    subOneFromTeamOneScore:function () {
+      if (this.teamOneScore > 0) {
+        this.teamOneScore--
+      }
+    },
+    subOneFromTeamTwoScore:function () {
+      if (this.teamTwoScore > 0) {
+        this.teamTwoScore--
+      }
+    },
   }
 }
 </script>
+
+<style scoped>
+.teams {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.team-score {
+  font-size: 48px;
+}
+
+.team-update-score-buttons {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
